c6ff3ab7636584a8654a2adf30e8f2f6f25a58e6
sajedegharibian <sajedegharibian@gmail.com>
2025-12-27 17:12:53 +0330

    Update products.js

--- JS/products.js
+++ JS/products.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const secEl=document.querySelector(".product");
+const secEl = document.querySelector(".product");
 
-(async()=>{
-    try {
-        const res=await fetch("https://fakestoreapi.com/products")
-        const data=await res.json()
-        data?.map(item=>{
-            secEl.innerHTML+=`<div class="pro-item"><div class="card"><h5>${item.title}</h5><p>${item.price}</p></div></div>`
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})                                                                                                     
+async () => {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    data?.map((item) => {
+      secEl.innerHTML += `<div class="pro-item"><div class="card"><h5>${item.title}</h5><p>${item.price}</p></div></div>`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
